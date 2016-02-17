870c691eba2a1147afcf8a282bf5036b10aabc68
Yu Hashizume <hashizume@yu-hashizume-no-macbook-pro.local>
2016-02-17 22:33:02 +0900

    新しい画面

---
+++ GitHubTest/subViewController.h
@@ -0,0 +1,13 @@
+//
+//  subViewController.h
+//  GitHubTest
+//
+//  Created by Yu Hashizume on 2016/02/17.
+//  Copyright © 2016年 Yu Hashizume. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface subViewController : UIViewController
+
+@end
