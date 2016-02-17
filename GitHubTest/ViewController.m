e804f7cd2685fb02849f056d650388873cca4359
Yu Hashizume <hashizume@yu-hashizume-no-macbook-pro.local>
2016-02-17 22:55:54 +0900

    まーじしたよ

--- GitHubTest/ViewController.m
+++ GitHubTest/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"あいうえお");
     NSLog(@"ブランチ追加したよ");
+    NSLog(@"branch");
 }
 
 - (void)didReceiveMemoryWarning {
